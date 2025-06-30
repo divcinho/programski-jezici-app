24edd13f10027ed6d38d59f63a237a088a95c088
divcinho <divcinho@gmail.com>
2025-06-30 19:30:28 +0200

    Generated the project

---
+++ src/main/java/com/example/programski_jezici_app/ProgramskiJeziciAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.programski_jezici_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramskiJeziciAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgramskiJeziciAppApplication.class, args);
+	}
+
+}
